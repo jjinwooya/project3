6dc51090c8136f8bc7b98164a73d7f934c9e0bbf
jjinwooya <jjinwoo1987@naver.com>
2024-08-08 12:55:50 +0900

    controller

---
+++ library-app/src/main/java/com/group/library_app/controller/home/HomeController.java
@@ -0,0 +1,12 @@
+package com.group.library_app.controller.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/v1/index.html";
+    }
+}
